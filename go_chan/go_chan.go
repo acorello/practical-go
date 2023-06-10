5d4b17e45a80f5e1769a22068a6ce670ba65c13e
am <am@acorello.it>
2023-06-10 16:24:40 +0200

    go_chan exercise

--- go_chan/go_chan.go
+++ go_chan/go_chan.go
@@ -2,74 +2,73 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	go fmt.Println("goroutine")
-	fmt.Println("main")
+	// go fmt.Println("goroutine")
+	// fmt.Println("main")
 
-	for i := 0; i < 3; i++ {
-		// Fix 2: Use a loop body variable
-		i := i // "i" shadows "i" from the for loop
-		go func() {
-			fmt.Println(i) // i from line 14
-		}()
+	// for i := 0; i < 3; i++ {
+	// 	// Fix 2: Use a loop body variable
+	// 	i := i // "i" shadows "i" from the for loop
+	// 	go func() {
+	// 		fmt.Println(i) // i from line 14
+	// 	}()
 
-		/* Fix 1: Use a parameter
-		go func(n int) {
-			fmt.Println(n)
-		}(i)
-		*/
-		/* BUG: All goroutines use the "i" for the for loop
-		go func() {
-			fmt.Println(i) // i from line 12
-		}()
-		*/
-	}
+	// 	/* Fix 1: Use a parameter
+	// 	go func(n int) {
+	// 		fmt.Println(n)
+	// 	}(i)
+	// 	*/
+	// 	/* BUG: All goroutines use the "i" for the for loop
+	// 	go func() {
+	// 		fmt.Println(i) // i from line 12
+	// 	}()
+	// 	*/
+	// }
 
-	time.Sleep(10 * time.Millisecond)
+	// time.Sleep(10 * time.Millisecond)
 
-	shadowExample()
+	// shadowExample()
 
-	ch := make(chan string)
-	go func() {
-		ch <- "hi" // send
-	}()
-	msg := <-ch // receive
-	fmt.Println(msg)
+	// ch := make(chan string)
+	// go func() {
+	// 	ch <- "hi" // send
+	// }()
+	// msg := <-ch // receive
+	// fmt.Println(msg)
 
-	go func() {
-		for i := 0; i < 3; i++ {
-			msg := fmt.Sprintf("message #%d", i+1)
-			ch <- msg
-		}
-		close(ch)
-	}()
+	// go func() {
+	// 	for i := 0; i < 3; i++ {
+	// 		msg := fmt.Sprintf("message #%d", i+1)
+	// 		ch <- msg
+	// 	}
+	// 	close(ch)
+	// }()
 
-	for msg := range ch {
-		fmt.Println("got:", msg)
-	}
+	// for msg := range ch {
+	// 	fmt.Println("got:", msg)
+	// }
 
-	/* for/range does this
-	for {
-		msg, ok := <-ch
-		if !ok {
-			break
-		}
-		fmt.Println("got:", msg)
-	}
-	*/
+	// /* for/range does this
+	// for {
+	// 	msg, ok := <-ch
+	// 	if !ok {
+	// 		break
+	// 	}
+	// 	fmt.Println("got:", msg)
+	// }
+	// */
 
-	msg = <-ch // ch is closed
-	fmt.Printf("closed: %#v\n", msg)
+	// msg = <-ch // ch is closed
+	// fmt.Printf("closed: %#v\n", msg)
 
-	msg, ok := <-ch // ch is closed
-	fmt.Printf("closed: %#v (ok=%v)\n", msg, ok)
+	// msg, ok := <-ch // ch is closed
+	// fmt.Printf("closed: %#v (ok=%v)\n", msg, ok)
 
 	// ch <- "hi" // ch is closed -> panic
 	values := []int{15, 8, 42, 16, 4, 23}
-	fmt.Println(sleepSort(values))
+	fmt.Println(SleepSort(values))
 }
 
 /* Channel semantics
--- go_chan/go_chan_exercise.go
+++ go_chan/go_chan_exercise.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"time"
+)
+
 /*
 For every value "n" in values, spin a goroutine that will
 - sleep "n" milliseconds
@@ -7,5 +11,39 @@ For every value "n" in values, spin a goroutine that will
 
 In the function body, collect values from the channel to a slice and return it.
 */
+func SleepSort(values []int) (res []int) {
+	ch := make(chan int)
+	for _, v := range values {
+		go /* HERE I SPIN-OFF! */ func(n int) {
+			time.Sleep(time.Duration(n) * time.Millisecond)
+			// concurrent write to channel means I can't close it right after the for loop: some coroutine may be writing into it.
+			ch <- n
+		}(v)
+	}
+	// I can't close the channel in any of the mandating goroutines; because they can't know when they're all done.
+	// I can only use a countdown, which in this case could be the length of the slice
+	// Can the length of the slice be changed so that id does not match the number of goroutines that were launched?
+	for range values {
+		v, open := <-ch
+		if !open {
+			break
+		}
+		res = append(res, v)
+	}
+	return res
+}
 
-func sleepSort(values []int) []int
+func SleepSortWithSync(values []int) (res []int) {
+	ch := make(chan int)
+	for _, v := range values {
+		go /* HERE I SPIN-OFF! */ func(n int) {
+			time.Sleep(time.Duration(n) * time.Millisecond)
+			// concurrent write to channel means I can't close it right after the for loop: some coroutine may be writing into it.
+			ch <- n
+		}(v)
+	}
+	for range values {
+		res = append(res, <-ch)
+	}
+	return res
+}
